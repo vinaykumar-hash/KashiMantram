d6f73d141ca060523f574a6ace5dd344d64b6193
vinaykumar-hash <vinaykumar7525@gmail.com>
2024-07-10 19:49:45 +0530

    ng

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Aboutus = () => {
   return (
-    <div style={{background:"#F0F0F0"}} className='w-full font-main flex justify-center items-center flex-col py-10'>
+    <div style={{background:"#F0F0F0"}} className='w-full font-main flex justify-center items-center flex-col py-10 '>
         <h2 className='text-2xl text-base-500 tracking-wide'>Kashi Mantram</h2>
         <h2 className='text-6xl text-base-500 '>About Us</h2>
         <div className='w-60 h-1 bg-base-500 flex justify-center items-center mt-2'>
             <div className='w-5 h-5 bg-base-500 rotate-45'></div>
         </div>
         <div className='flex justify-center items-center mx-40 gap-20 mt-10'>
-            <p className='font-s'>Hotel Kashi Mantram, located at the spiritual capital of India, Varanasi. Best Luxury hotel with ample facility and clean rooms and bathrooms, located in prime location of Manduadih road, Varanasi. The hotel is around 3 kms from Shri Kashi Vishwanath Temple.</p>
-            <div className='min-w-80 h-60 bg-gray-500 border-base-500 border-l-4'></div>
+            <p className='font-s text-xl flex-1 text-center'>Hotel Kashi Mantram, located at the spiritual capital of India, Varanasi. Best Luxury hotel with ample facility and clean rooms and bathrooms, located in prime location of Manduadih road, Varanasi. The hotel is around 3 kms from Shri Kashi Vishwanath Temple.</p>
+            <div className='flex-1 min-w-80 h-60 bg-gray-500 border-base-500 border-l-4'></div>
         </div>
     </div>
   )
